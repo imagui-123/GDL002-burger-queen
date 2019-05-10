42d7ba79497c230f6302ab75346ed67cb95eb6a1
Iris <iaguiarg@hotmail.com>
2019-05-10 13:17:10 -0500

    Updates

---
+++ precache-manifest.6cf57a4e88c0a8307f20ca4ab5b09228.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e2adda0d68cba1f08c6d1e5b1e3ddc",
+    "url": "/imagui-123/GDL002-burger-queen/index.html"
+  },
+  {
+    "revision": "0efb7610a106dc367703",
+    "url": "/imagui-123/GDL002-burger-queen/static/css/main.d2696516.chunk.css"
+  },
+  {
+    "revision": "c343ce630bce0c5198ca",
+    "url": "/imagui-123/GDL002-burger-queen/static/js/2.e09f9078.chunk.js"
+  },
+  {
+    "revision": "0efb7610a106dc367703",
+    "url": "/imagui-123/GDL002-burger-queen/static/js/main.be88b9b9.chunk.js"
+  },
+  {
+    "revision": "0e37b7fa3e4b7e83f318",
+    "url": "/imagui-123/GDL002-burger-queen/static/js/runtime~main.458f1422.js"
+  }
+]);
